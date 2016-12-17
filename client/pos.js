a7854d1efccd18099f357876ac5c273bcc4ce605
phtn <phtn458@gmail.com>
2016-12-17 00:46:21 -0500

    final toolbar

--- client/pos.js
+++ client/pos.js
@@ -7,6 +7,8 @@ import Avatar from 'material-ui/Avatar'
 import Paper from 'material-ui/Paper'
 import Logo from 'material-ui/svg-icons/av/subtitles'
 import {red400} from 'material-ui/styles/colors';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import RaisedButton from 'material-ui/RaisedButton'
 
 const styles = {
   chip: {
@@ -23,6 +25,16 @@ const styles = {
   	fontFamily: 'Quicksand' ,
   	fontSize: '40px',
   	fontWeight: 700
+  },
+  tabs: {
+  	backgroundColor: '#333 !important',
+  	margin: 4
+  },
+  cat: {
+  	fontFamily: 'Poppins, sans-serif',
+  	fontWeight: 700,
+  	fontSize: '1em !important',
+  	color: '#333 !important'
   }
 }
 
@@ -36,9 +48,10 @@ export default class POS extends Component {
 			<Card className="top-card">
 				<Paper 
 					zDepth={2} 
-					children={<div className="brand-div"><Logo color={red400}/>NANOS</div>}
-				/>
+					children={<div className="brand-div"><Logo color={red400}/>NANOS</div>}/>
 				
+				
+
 				<div style={styles.wrapper}>
 					<Chip style={styles.chip}>
 						<Avatar />
@@ -53,8 +66,39 @@ export default class POS extends Component {
 				
 			</Card>
 
+			<div>
+        <Toolbar style={styles.tabs}>
+
+        <ToolbarGroup firstChild={true}>
+          <FlatButton labelStyle={styles.cat} label="Tap" secondary={true}/>
+        </ToolbarGroup>
+
+        <ToolbarGroup>  
+          <FlatButton labelStyle={styles.cat} label="Bottle" secondary={true}/>
+        </ToolbarGroup>
+
+        <ToolbarGroup lastChild={true}>  
+          <FlatButton labelStyle={styles.cat} label="shots" secondary={true}/>
+        </ToolbarGroup>
+
+        <ToolbarGroup lastChild={true}>  
+          <FlatButton labelStyle={styles.cat} label="mix" secondary={true}/>
+        </ToolbarGroup>
+
+        <ToolbarGroup lastChild={true}>  
+          <FlatButton labelStyle={styles.cat} label="wine" secondary={true}/>
+        </ToolbarGroup>
+
+        <ToolbarGroup lastChild={true}>  
+          <FlatButton labelStyle={styles.cat} label="soda" secondary={true}/>
+        </ToolbarGroup>
+          
+        </Toolbar>
+	    </div>
+
+
 				<GridList cols={4} >
-					<Card className="cards">
+					<Card className="cards" id="corona">
 						<span className="item-header">1</span>
 					</Card>
 					<Card className="cards">
@@ -62,6 +106,12 @@ export default class POS extends Component {
 					</Card>
 					<Card className="cards">
 						<span className="item-header">3</span>
+					</Card>
+					<Card className="cards">
+						<span className="item-header">4</span>
+					</Card>
+					<Card className="cards">
+						<span className="item-header">5</span>
 					</Card>
 				</GridList>
 			</div>
