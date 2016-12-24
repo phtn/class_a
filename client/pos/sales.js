3e5a976f90e264bb61904a88b90936e0386ae8f8
phtn458 <phtn458@gmail.com>
2016-12-23 21:02:39 -0500

    added sales table

--- client/pos/sales.js
+++ client/pos/sales.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
   from 'material-ui/Table'
+import Mui from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import dark from 'material-ui/styles/baseThemes/darkBaseTheme'
+
 
 const Sales = () => (
+  <Mui muiTheme={getMuiTheme(dark)}>
   <Table>
-    <TableHeader>
-      <TableHeaderColumn>item</TableHeader>
-
-
+    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+    <TableRow>
+      <TableHeaderColumn>#</TableHeaderColumn>
+      <TableHeaderColumn>NAME</TableHeaderColumn>
+      <TableHeaderColumn>TOTAL</TableHeaderColumn>
+      <TableHeaderColumn>TENDERED</TableHeaderColumn>
+      <TableHeaderColumn>CHANGE</TableHeaderColumn>
+      <TableHeaderColumn colSpan={3}>ITEMS</TableHeaderColumn>
+    </TableRow>
     </TableHeader>
   </Table>
+  </Mui>
 )
 export default Sales
